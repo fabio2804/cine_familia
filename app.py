902d7154c78cd2095e7429b56338231d387adf0c
Lucas Ramos Silva <lucas.silva682@fatec.sp.gov.br>
2021-11-23 01:35:23 -0300

    Rotas, scripts e nome da aplicação

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from flask import Flask, render_template, url_for, Blueprint
 app = Flask(__name__)
 
 
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
