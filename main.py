52a5bf4fadf2ad4cf5755c2a6328ce75c20b9d7c
Fábio Vilela <fabiovilela123@gmail.com>
2021-11-23 00:33:20 -0300

    refac: adding auto_reloader to main

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app import app
 
 if __name__ == '__main__':
     print('Acesse o seu site em http://localhost:4200')
-    app.run(host='0.0.0.0', port=4200, debug=True)
+    app.run(host='0.0.0.0', port=4200, debug=True, use_reloader=True)
